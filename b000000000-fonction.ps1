5855e7521a2aafa767f9ab89706c235e988815eb
ordenrosae <ordenrosae@yahoo.fr>
2020-04-02 17:25:52 -0400

    .

--- b000000000-fonction.ps1
+++ b000000000-fonction.ps1
@@ -13,15 +13,19 @@
 
 # Definition de la fonction
   function Stagiaire { 
+      [CmdletBinding()]
       param (
          [String]$personneNom,
          [Int]$personneAge
       )
   # message de bienvenue 
-  "Bonjour {0} ! Tu as {1} ans." -F $personneNom, $personneAge
+    BEGIN {Write-Verbose "Début du script"}
+    PROCESS { "Bonjour {0} ! Tu as {1} ans." -F $personneNom, $personneAge }
+    END {Write-Verbose "Fin du script"}
    
    }
  
 # Appel de la fonction
 Stagiaire Toronto 35
+Stagiaire "Pascal Siakam" 26  -verbose
 
